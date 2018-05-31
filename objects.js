337724dc6ca865879575502b1855fe1e8ac4768d
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-31 07:05:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 var playlist = {someone:"something"}
+=======
+var playlist = {}
+>>>>>>> 9a04943cbecc1a7be9765671728dcc1a304f24df
 
 
 function updatePlaylist(playlist, artist, song){
   
+<<<<<<< HEAD
   playlist[artist] = song
   return playlist
   
@@ -12,4 +17,8 @@ function removeFromPlaylist(playlist, artist) {
   
   delete playlist[artist]
   return playlist
+=======
+  return Object.assign({}, playlist, { [artist]:[song] })
+  
+>>>>>>> 9a04943cbecc1a7be9765671728dcc1a304f24df
 }
